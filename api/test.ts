b73b34ff38680d10164d87d7397513021c586662
MrChnaydar <mourak.nizar@gmail.com>
2025-04-29 18:33:20 +0200

    changed test.ts

--- api/test.ts
+++ api/test.ts
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-  res.status(200).json({ msg: "Plain JS test working" });
+module.exports = (req: any, res: any) => {
+  res.status(200).json({ msg: 'Plain JS test working' });
 };
