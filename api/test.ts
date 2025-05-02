27ee8f73205a046ff8d27da90790d29d75af75c0
MrChnaydar <mourak.nizar@gmail.com>
2025-05-02 18:33:01 +0200

    changed more tailwindcss

--- api/test.ts
+++ api/test.ts
@@ -1,3 +1,5 @@
+console.log('test 2');
 module.exports = (req: any, res: any) => {
+  console.log('test 1');
   res.status(200).json({ msg: 'Plain JS test working' });
 };
--- src/app/services/key.service.ts
+++ src/app/services/key.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class KeyService {
-  public api = '14165bbcf9dd417ddc8ee87ecd6fbe32';
+  // public api = '14165bbcf9dd417ddc8ee87ecd6fbe32'; firs api key
+  public api = 'a32cbeee0f3729d3e97a90e998d57368';
   // public getMapKey() {
   //   return this.MAPKEY;
   // }
